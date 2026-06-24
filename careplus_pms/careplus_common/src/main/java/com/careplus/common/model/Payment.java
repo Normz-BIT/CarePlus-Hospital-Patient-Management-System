c799204e1fae7bafa9a1b5218a7c1449e539a2b7
Norman Martin <normaneverthonmartin@gmail.com>
2026-06-24 00:00:25 -0500

    feat(Payment):added base class

---
+++ careplus_pms/careplus_common/src/main/java/com/careplus/common/model/Payment.java
@@ -0,0 +1,79 @@
+package com.careplus.common.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/*
+ * Payment class
+ */
+public class Payment implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int paymentId;
+	private double amountPaid;
+	private double outstandingBalance;
+	private String description;
+
+	public Payment() {
+
+	}
+
+	public Payment(int paymentId, double amountPaid, double outstandingBalance, String description) {
+		this.paymentId = paymentId;
+		this.amountPaid = amountPaid;
+		this.outstandingBalance = outstandingBalance;
+		this.description = description;
+	}
+
+	public int getPaymentId() {
+		return paymentId;
+	}
+
+	public void setPaymentId(int paymentId) {
+		this.paymentId = paymentId;
+	}
+
+	public double getAmountPaid() {
+		return amountPaid;
+	}
+
+	public void setAmountPaid(double amountPaid) {
+		this.amountPaid = amountPaid;
+	}
+
+	public double getOutstandingBalance() {
+		return outstandingBalance;
+	}
+
+	public void setOutstandingBalance(double outstandingBalance) {
+		this.outstandingBalance = outstandingBalance;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Payment)) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Payment other = (Payment) obj;
+		return paymentId == other.paymentId;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(paymentId);
+	}
+
+}
