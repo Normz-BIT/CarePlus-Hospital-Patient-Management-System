21bca1778def66c67bdc289ac6c1f5919b39e5de
Norman Martin <normaneverthonmartin@gmail.com>
2026-07-07 03:26:29 -0500

    fix(ClientServer): renamed to Client and simplified logic

--- careplus_pms/careplus_client_employee/src/main/java/com/careplus/client/employee/controller/NurseController.java
+++ careplus_pms/careplus_client_employee/src/main/java/com/careplus/client/employee/controller/NurseController.java
@@ -1,13 +1,13 @@
 package com.careplus.client.employee.controller;
 
-import com.careplus.common.client.net.ServerConnection;
+import com.careplus.common.client.net.Client;
 import com.careplus.common.model.Nurse;
 
 public class NurseController extends EmployeeController{
 
 	// private final DoctorView view;
 
-    public NurseController(MainController main, ServerConnection connection, Nurse nurse) {
+    public NurseController(MainController main, Client connection, Nurse nurse) {
         super(main, connection, nurse);
        // this.view = new NurseView(nurse);
        // this.view.setLogoutHandler(this::logout);
